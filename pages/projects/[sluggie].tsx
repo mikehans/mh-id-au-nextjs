4a04a587d54982c9e5c9434355569fc83906b83c
Mike Hansford <mjhansford@gmail.com>
2022-03-17 19:02:41 +1100

    Add statically compiled projects pages

--- pages/projects/[sluggie].tsx
+++ pages/projects/[sluggie].tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import dotenv from 'dotenv'
+
 
 function ProjectPage() {
   return (
@@ -7,3 +9,32 @@ function ProjectPage() {
 }
 
 export default ProjectPage
+
+export async function getStaticPaths() {
+  dotenv.config();
+
+  const url = `${process.env.API_URL}/projects`;
+  const res = await fetch(url);
+  const posts = await res.json();
+  
+  const paths = posts.map(p => ({
+    params: {sluggie: p.sluggie}
+  }));
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params: {sluggie}}){
+  dotenv.config();
+
+  const url = `${process.env.API_URL}/projects/${sluggie}`;
+  const res = await fetch(url);
+  const post = await res.json()
+
+  return {
+    props: {post}
+  }
+}
