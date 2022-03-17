4b212cc24a5289909b7b25d42ed20f530270b799
Mike Hansford <mjhansford@gmail.com>
2022-03-17 19:20:20 +1100

    Add projects list to Projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import dotenv from 'dotenv'
+import LatestItems from "../../components/LatestItems";
 
-function ProjectsPage() {
+function ProjectsPage({data}) {
   return (
+    <>
     <div>Projects</div>
+    <LatestItems title="Latest Projects" list={data} path="projects" />
+    </>
   )
 }
 
 export default ProjectsPage
+
+export async function getStaticProps(){
+  dotenv.config();
+
+  const url = `${process.env.API_URL}/projects?_sort=published_at:DESC`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return {
+    props: {
+      data,
+      revalidate: 20
+    }
+  }
+}
