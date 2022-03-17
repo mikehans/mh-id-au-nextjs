a708c567ed0166f7531169ad377049c12cd20110
Mike Hansford <mjhansford@gmail.com>
2022-03-17 21:44:33 +1100

    Card styling

--- components/LatestItem.tsx
+++ components/LatestItem.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import Link from 'next/link';
 import dateFormatter from "./utils/dateFormatter";
+import styles from "./LatestItem.module.css";
+import Image from "next/image";
 
-function LatestItem({published_at, title, description, sluggie, path}) {
+function LatestItem({published_at, title, description, sluggie, path, image}) {
   const buildUrl = (p: string, s:string) => {
     return `/${path}/${s}`;
   }
 
   return (
-    <article>
-        <h3>{title}</h3>
-        <p>{dateFormatter(published_at, "short")}</p>
-        <p>{description}</p>
-        <Link href={buildUrl(path, sluggie)}><a>Read more</a></Link>
+    <article className={styles.card}>
+        <h3 className={styles.heading}>{title}</h3>
+        <p className={styles.date}>{dateFormatter(published_at, "short")}</p>
+        <p className={styles.description}>{description}</p>
+        <Link href={buildUrl(path, sluggie)}><a className={styles.link}>Read more</a></Link>
+        {/* {image && image.length > 0 ? <p><Image src={image[0].url} width={200} height={100} alt={image[0].alternativeText} className={styles.image} /></p>: ""} */}
     </article>
   )
 }
--- components/LatestItemsList.tsx
+++ components/LatestItemsList.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import LatestItem from "./LatestItem"
+import styles from "./LatestItemsList.module.css"
 
 function LatestItemsList({list, path}) {
+  console.log('list :>> ', list);
   return (
     <section>
-        <ul>
+        <ul className={styles.cardList}>
         {
             list.map((item, idx) => (
-                <li key={idx}><LatestItem {...item} path={path} /></li>
+                <li key={idx} className={styles.cardListItem}><LatestItem {...item} path={path} /></li>
             ))
         }
         </ul>
