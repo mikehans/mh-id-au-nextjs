b6132da97247baffecff85b39f5151c8185351d1
Mike Hansford <mjhansford@gmail.com>
2022-11-01 00:51:34 +1100

    Re-connect Blogs to data

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { markdownToHtml } from "../../components/utils/markdownToHtml";
+import { parseFrontMatter } from "../../components/utils/frontmatterFromMarkdown";
+import dateFormatter from "../../components/utils/dateFormatter";
+import dotenv from "dotenv";
+import parse from "html-react-parser";
+import matter from "gray-matter";
+import fs from 'fs';
+import path from 'path';
+
+function BlogPostPage(props) {
+  const [bodyContent, setBodyContent] = useState("");
+  const [frontMatter, setFrontMatter] = useState({});
+
+  console.log("post :>> ", props);
+
+  useEffect(() => {
+    // setFrontMatter(parseFrontMatter(post.content));
+    setBodyContent(markdownToHtml(props.content));
+  }, [props.content]);
+
+  return (
+    <article>
+      <h2>{props.frontmatter.title}</h2>
+      <div>Published: {dateFormatter(props.frontmatter.date, "long")}</div>
+      <div>{parse(bodyContent)}</div>
+    </article>
+  );
+}
+
+export default BlogPostPage;
+
+export async function getStaticPaths() {
+  dotenv.config();
+
+  const posts = fs.readdirSync("./data/posts");
+  const paths = posts.map((post) => {
+    const { data } = matter(fs.readFileSync(path.join("./data/posts", post)));
+    console.log('data', data)
+    return { params: {slug: data.slug}};
+  });
+
+  console.log('getStaticPaths paths', paths)
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+// I can't detach the markdown filename from the slug param
+export async function getStaticProps({params: {slug}}){
+  console.log('slug :>> ', slug);
+
+  const markdownWithMetadata = fs.readFileSync(path.join('./data/posts', slug + '.md' ), 'utf-8');
+  console.log('markdownWithMetadata :>> ', markdownWithMetadata);
+
+  const {data: frontmatter, content} = matter(markdownWithMetadata);
+
+  return {
+    props: {frontmatter, content}
+    // props: {}
+  }
+}
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import dotenv from 'dotenv'
+import React from "react";
+import dotenv from "dotenv";
 import LatestItems from "../../components/LatestItems";
+import matter from "gray-matter";
+import fs from "fs";
+import path from "path";
 
-
-function BlogPage({data}: {data:any}) {
-  console.log('data :>> ', data);
+function BlogPage({ data }: { data: any }) {
+  // console.log("data :>> ", data);
   return (
     <>
       <h2>Tech Blog</h2>
-      <p>I blog about various topics relevant to web development and other software topics in general.</p>
+      <p>
+        I blog about various topics relevant to web development and other
+        software topics in general.
+      </p>
       <LatestItems title="My posts" list={data} path="blog" />
     </>
-  )
+  );
 }
 
-export default BlogPage
+export default BlogPage;
 
-export async function getStaticProps(){
+export async function getStaticProps() {
   dotenv.config();
 
-  const postsUrl = `${process.env.API_URL}/posts?_sort=published_at:DESC`;
-  const postsResponse = await fetch(postsUrl);
+  const files = fs.readdirSync("./data/posts");
+
+  console.log("files", files);
+
+  const fileInfo = files.map((file) => {
+    const f = fs.readFileSync(path.join("./data/posts", file));
+    const { data, content } = matter(f);
+    return { data, content };
+  });
 
   return {
     props: {
-      data: await postsResponse.json(),
-      revalidate: 20
+      data: fileInfo
     }
-  }
+  };
 }
