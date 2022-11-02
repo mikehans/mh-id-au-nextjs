c448f131fd2c83d103ee30144a8186a7cc2afb8e
Mike Hansford <mjhansford@gmail.com>
2022-11-02 19:05:14 +1100

    More clean up

--- components/utils/markdownToHtml.ts
+++ components/utils/markdownToHtml.ts
@@ -1,15 +1,5 @@
 import { marked } from "marked";
-// import { remark } from "remark";
-// import remarkRehype from "remark-rehype";
-// import rehypeStringify from "rehype-stringify";
-
 
 export function markdownToHtml(markdownContent: string): string {
-//   let result;
-  //   result = await remark()
-  //     .use(remarkRehype)
-  //     .use(rehypeStringify)
-  //     .process(markdownContent);
-
   return marked(markdownContent);
 }
