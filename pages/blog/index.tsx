c9c12408d624364f06204ee20362bee2a00094e3
Mike Hansford <mjhansford@gmail.com>
2022-11-09 12:39:08 +1100

    Add date sort to blog index page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,6 +5,7 @@ import matter from "gray-matter";
 import fs from "fs";
 import path from "path";
 import { BlogFrontmatter } from "../../types/BlogInterfaces";
+// import { sortByDateDesc } from "../../components/utils/sortAlgos";
 
 export interface BlogPageProps {
   data: BlogFrontmatter;
@@ -26,6 +27,22 @@ function BlogPage({ data }: { data: BlogFrontmatter }) {
 
 export default BlogPage;
 
+type Post = {
+  data: any;
+  content: string;
+  date: string;
+};
+
+function descSort(entry1: Post, entry2: Post): number {
+  if (new Date(entry1.date) < new Date(entry2.date)) {
+    return 1;
+  }
+  if (new Date(entry1.date) > new Date(entry2.date)) {
+    return -1;
+  }
+  return 0;
+}
+
 export async function getStaticProps() {
   dotenv.config();
   const postsPath = path.join(process.env.DATA_PATH as string, process.env.POSTS_PATH as string);
@@ -37,12 +54,12 @@ export async function getStaticProps() {
   const fileInfo = files.map((file) => {
     const f = fs.readFileSync(path.join(postsPath, file));
     const { data, content } = matter(f);
-    return { data, content };
+    return { data, content, date: data.date };
   });
 
   return {
     props: {
-      data: fileInfo
+      data: fileInfo.sort(descSort)
     }
   };
 }
